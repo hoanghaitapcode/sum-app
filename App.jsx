8ff29b381116e8c4eed5b246eeee47a5595a8cbd
HaiHo <145896354+hoanghaitapcode@users.noreply.github.com>
2025-11-05 16:32:19 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SumCalculator from "./components/SumCalculator";
+import "./App.css"; // Import file CSS
+
+function App() {
+  return (
+    <div className="App">
+      <SumCalculator />
+    </div>
+  );
+}
+
+export default App;
